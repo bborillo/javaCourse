f679588b421f79fe363f4cc371106964c19772d3
Boris <vint_c@mail.ru>
2016-03-09 16:54:28 +0600

    create HTML

--- JScript/src/script.js
+++ JScript/src/script.js
@@ -1,8 +1,7 @@
 /**
  * Created by User on 25.02.2016.
  */
-var multiString="";
-var outString;
+var multiString = "";
 for (i = 0; i <= 10; i++) {
     multiString = multiString + "строка" + " " + i + "\n";
 }
