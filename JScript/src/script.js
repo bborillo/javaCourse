879298f4710b29882b71195c95467db3e5d8b262
Boris <vint_c@mail.ru>
2016-03-09 16:54:10 +0600

    create HTML

--- JScript/src/script.js
+++ JScript/src/script.js
@@ -1,16 +1,9 @@
 /**
  * Created by User on 25.02.2016.
  */
-var multiString = "строка 1\
-строка 2\
-строка 3\
-строка 4\
-строка 5\
-строка 6\
-строка 7\
-строка 8\
-строка 9\
-строка 10";
-
-alert (multiString);
-console.log(multiString);
+var multiString="";
+var outString;
+for (i = 0; i <= 10; i++) {
+    multiString = multiString + "строка" + " " + i + "\n";
+}
+alert(multiString);
