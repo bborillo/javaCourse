d040c639508d281e5cd148cad73ab68b38a50f46
Boris <vint_c@mail.ru>
2016-03-10 15:46:40 +0600

    create HTML

--- JScript/src/squareCalc.js
+++ JScript/src/squareCalc.js
@@ -9,10 +9,9 @@ var discriminant = calcDiscriminant(a, b, c);
 console.log("Дискрименант: " + discriminant);
 if (discriminant > 0) {
     var x = new Array();
-    x = calcTwoResults(a, b,discriminant);
+    x = calcTwoResults(a, b, discriminant);
     console.log("X1=" + x[0] + ", X2=" + x[1]);
-}
-else if (discriminant == 0) {
+} else if (discriminant == 0) {
     var x = calcOneResult(a, b);
     console.log("X1=X2=" + x);
 } else {
@@ -21,18 +20,15 @@ else if (discriminant == 0) {
 
 
 function calcDiscriminant(a, b, c) {
-    var discriminant = Math.pow(b, 2) - 4 * a * c;
-
-    return discriminant;
+    return Math.pow(b, 2) - 4 * a * c;
 }
 
-function calcTwoResults(a, b,discriminant ) {
+function calcTwoResults(a, b, discriminant) {
     var x = new Array();
     x[0] = (-b + Math.sqrt(discriminant)) / (2 * a);
     x[1] = (-b - Math.sqrt(discriminant)) / (2 * a);
     return x;
 }
 function calcOneResult(a, b) {
-    var x = -b / 2 * a;
-    return x;
+    return -b / (2 * a);
 }
