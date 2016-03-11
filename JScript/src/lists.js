c673879bc83f28cef25b4bbf53af43d1cd2fd40b
Boris <vint_c@mail.ru>
2016-03-11 08:44:36 +0600

    create HTML

--- JScript/src/lists.js
+++ JScript/src/lists.js
@@ -4,22 +4,22 @@
 var listNumbers = [1, 5, 7, 4, 2, 7, 7, 3, 2];
 listNumbers.reverse(listNumbers.sort(listNumbers));
 console.log(listNumbers);
-var subListFirst5 = listNumbers.slice(0, 4);
+var subListFirst5 = listNumbers.slice(0, 5);
 console.log("First 5 elements: " + subListFirst5);
-var subListLast5 = listNumbers.slice(listNumbers.length - 5, listNumbers.length);
+var subListLast5 = listNumbers.slice(listNumbers.length - 5);
 console.log("Last 5 elements: " + subListLast5);
 
-var x = 0;
+var evenNumbersSumm = 0;
 for (var i = 0; i < listNumbers.length; i++) {
     if (listNumbers[i] % 2 == 0) {
-        x += listNumbers[i];
+        evenNumbersSumm += listNumbers[i];
     }
 }
-console.log("Сумма четных чисел " + x);
+console.log("Сумма четных чисел " + evenNumbersSumm);
 
 var sortListOneHundred = new Array();
-for (var i = 1; i <= 8; i++) {
-    sortListOneHundred[i] = i;
+for (var i = 0; i < 100; i++) {
+    sortListOneHundred[i] = i + 1;
 }
 console.log(sortListOneHundred);
 
